ec12f09aeb97dd7a23f1b74db97e1aa341238b8a
Talpha Harris <ancientpathcm@gmail.com>
2026-06-19 20:33:01 -0400

    board

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        board: resolve(__dirname, "board.html")
+      }
+    }
+  },
   server: {
     port: 5173,
     strictPort: false
